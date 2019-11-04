39b34c7f5fd456b4961594d0fac47f4a5cb946fc
Liew Syet Chau <syetchau@hotmail.com>
2019-11-04 21:53:44 +0800

    Update APIService.java

--- app/src/main/java/com/example/liew/idelivery/Remote/APIService.java
+++ app/src/main/java/com/example/liew/idelivery/Remote/APIService.java
@@ -13,7 +13,7 @@ public interface APIService {
     @Headers(
             {
                     "Content-Type:application/json",
-                    "Authorization:key=AAAA-I4Eb74:APA91bHhEpGVC-ASTX8K2KHd4L3S3q4Yv_JTqapb6XHwX5rE8zo9F12FQM8R8cl5VgFu-G2_zDK4gAonoPDPb2yMGpUkui43HatR9qggJua5hq_EmNOmAJtB0a3WfJNUX8WawQqFFn6L"
+                    "Authorization:key=yourKey"
             }
 
     )
